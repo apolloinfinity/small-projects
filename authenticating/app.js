b5b51a4871b30b1f3da4d6ea3522b46a06e61846
Javier Ramirez <apollo.infinity88@gmail.com>
2019-09-24 10:49:03 -0700

    Added core modules, functions and middleware to inside of an asynchronous function

--- authenticating/app.js
+++ authenticating/app.js
@@ -9,23 +9,22 @@ const start = async () => {
   try {
     await mongoose.connect(db, { useNewUrlParser: true });
     console.log('Connected to Mongo');
+    const app = express();
+    app.set('view engine', 'ejs');
+
+    const user = require('./routes/indexRoute');
+
+    // Middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    // Routes
+    app.use('/', user);
+
+    app.listen(PORT, console.log(`App started on port ${PORT}`));
   } catch (err) {
     console.error(err);
   }
 };
-
-const app = express();
-app.set('view engine', 'ejs');
-
-const user = require('./routes/index');
-
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-app.use('/', user);
-
-app.listen(PORT, console.log(`App started on port ${PORT}`));
 
 start();
