1ef8ae174c3d11becca4f0bb874bf1c1f6016ad5
Javier Ramirez <apollo.infinity88@gmail.com>
2019-11-19 13:11:08 -0800

    Updating packages

--- authenticating/app.js
+++ authenticating/app.js
@@ -6,25 +6,25 @@ const PORT = process.env.PORT || 80;
 const db = require('./config/db').mongoURI;
 
 const start = async () => {
-  try {
-    await mongoose.connect(db, { useNewUrlParser: true });
-    console.log('Connected to Mongo');
-    const app = express();
-    app.set('view engine', 'ejs');
+	try {
+		await mongoose.connect(db, { useNewUrlParser: true });
+		console.log('Connected to Mongo');
+		const app = express();
+		app.set('view engine', 'ejs');
 
-    const user = require('./routes/indexRoute');
+		const user = require('./routes/indexRoute');
 
-    // Middleware
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+		// Middleware
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({ extended: true }));
 
-    // Routes
-    app.use('/', user);
+		// Routes
+		app.use('/', user);
 
-    app.listen(PORT, console.log(`App started on port ${PORT}`));
-  } catch (err) {
-    console.error(err);
-  }
+		app.listen(PORT, console.log(`App started on port ${PORT}`));
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 start();
